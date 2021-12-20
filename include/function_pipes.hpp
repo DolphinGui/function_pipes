59cd926d3012c568d8189289aa9445ff54a6e905
DolphinGui <umeda.shin@gmail.com>
2021-12-20 14:09:03 -0800

    cleaned some stuff up

--- include/function_pipes.hpp
+++ include/function_pipes.hpp
@@ -32,20 +32,15 @@ constexpr decltype(auto) operator & (pipe<F1> function1, pipe<F2> function2) {
 
 template <typename Int, Int... ints, typename... tuple_types,
           typename... pipe_types>
-decltype(auto) unpack(std::integer_sequence<Int, ints...> int_seq,
+constexpr decltype(auto) unpack(std::integer_sequence<Int, ints...> int_seq,
             std::tuple<tuple_types...> tuple, pipe_types... pipes) {
   static_assert(sizeof...(tuple_types) == sizeof...(pipe_types),
                 "number of functions does not match tuple members");
   return std::make_tuple((pipes(std::get<ints>(tuple)))...);
 }
 template <typename... tuple_types, typename... pipe_types>
-decltype(auto) unpack(std::tuple<tuple_types...> tuple, pipe_types... pipes) {
+constexpr decltype(auto) unpack(std::tuple<tuple_types...> tuple, pipe_types... pipes) {
   return unpack(std::make_integer_sequence<int, sizeof...(tuple_types)>(),
                 tuple, pipes...);
 }
-template<typename... args>
-pipe nullpipe([](args...){return;});
-// std::apply([](auto ...x)
-// {std::make_tuple(some_function(x)...);} , the_tuple);
-template <typename funct> using function_pipe = pipe<funct>;
 } // namespace mtx
