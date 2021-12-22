5d6fd0aeb86518b612057fb47becca80503e80a9
DolphinGui <umeda.shin@gmail.com>
2021-12-22 14:49:04 -0800

    minor fix

--- conanfile.py
+++ conanfile.py
@@ -8,11 +8,15 @@ class function_pipes(ConanFile):
     description = "A header-only library for composing functions in c++17"
     license = "Unlicense"
     # No settings/options are necessary, this is header only
-    exports_sources = ["include/*"]
+    exports_sources = "include/*"
     no_copy_source = True
 
     def package(self):
         self.copy("*.hpp", dst="include", src="public")
 
-    def package_info(self):
+    def package_id(self):
         self.info.header_only()
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "function_pipes"
+        self.cpp_info.names["cmake_find_package_multi"] = "function_pipes"
